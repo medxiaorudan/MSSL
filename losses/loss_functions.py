31990f78b233d816a1b24729a2fb2120cd6ff3b4
medxiaorudan <729323503@qq.com>
2023-09-25 23:46:42 +0200

    Add files via upload

--- losses/loss_functions.py
+++ losses/loss_functions.py
@@ -1,1 +1,416 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.module import Module
+import numpy as np
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class RecallCrossEntropy(torch.nn.Module):
+    def __init__(self, n_classes=2, ignore_index=0):
+        super(RecallCrossEntropy, self).__init__()
+        self.n_classes = n_classes
+        self.ignore_index = ignore_index
+
+    def forward(self, input, target): 
+
+        pred = input.argmax(1)
+        idex = (pred != target).view(-1) 
+        
+        gt_counter = torch.ones((self.n_classes,)).cuda() 
+        gt_idx, gt_count = torch.unique(target,return_counts=True)
+        
+        gt_count[gt_idx==self.ignore_index] = gt_count.clone()[0]
+        gt_idx[gt_idx==self.ignore_index] = 1 
+        gt_counter[gt_idx] = gt_count.float()
+        
+        #calculate false negative counts
+        fn_counter = torch.ones((self.n_classes)).cuda() 
+        fn = target.view(-1)[idex.long()]
+        fn_idx, fn_count = torch.unique(fn,return_counts=True)
+        
+        # map ignored label to an exisiting one
+        fn_count[fn_idx==self.ignore_index] = fn_count.clone()[0]
+        fn_idx[fn_idx==self.ignore_index] = 1 
+        fn_counter[fn_idx] = fn_count.float()
+        
+        weight = fn_counter / gt_counter
+        
+        CE = F.cross_entropy(input, target, reduction='none',ignore_index=self.ignore_index)
+        loss =  weight[target] * CE
+        return loss.mean()
+
+
+class RecallLoss(nn.Module):
+    """ An unofficial implementation of
+        <Recall Loss for Imbalanced Image Classification and Semantic Segmentation>
+        Created by: Zhang Shuai
+        Email: shuaizzz666@gmail.com
+        recall = TP / (TP + FN)
+    Args:
+        weight: An array of shape [C,]
+        predict: A float32 tensor of shape [N, C, *], for Semantic segmentation task is [N, C, H, W]
+        target: A int64 tensor of shape [N, *], for Semantic segmentation task is [N, H, W]
+    Return:
+        diceloss
+    """
+    def __init__(self, weight=None):
+        super(RecallLoss, self).__init__()
+        if weight is not None:
+            weight = torch.Tensor(weight)
+            self.weight = weight / torch.sum(weight) # Normalized weight
+        self.smooth = 1e-5
+
+    def forward(self, input, target):
+        N, C = input.size()[:2]
+        _, predict = torch.max(input, 1)# # (N, C, *) ==> (N, 1, *)
+
+        print("predict.shape:",predict.shape)
+        print("predict:",predict)
+
+        predict = predict.view(N, 1, -1) # (N, 1, *)
+        target = target.view(N, 1, -1) # (N, 1, *)
+        last_size = target.size(-1)
+
+        ## convert predict & target (N, 1, *) into one hot vector (N, C, *)
+        predict_onehot = torch.zeros((N, C, last_size)).cuda() # (N, 1, *) ==> (N, C, *)
+        predict_onehot.scatter_(1, predict, 1) # (N, C, *)
+        target_onehot = torch.zeros((N, C, last_size)).cuda() # (N, 1, *) ==> (N, C, *)
+        target_onehot.scatter_(1, target, 1) # (N, C, *)
+
+        true_positive = torch.sum(predict_onehot * target_onehot, dim=2)  # (N, C)
+        total_target = torch.sum(target_onehot, dim=2)  # (N, C)
+        ## Recall = TP / (TP + FN)
+        recall = (true_positive + self.smooth) / (total_target + self.smooth)  # (N, C)
+        
+        print("recall:",recall)
+        
+        if hasattr(self, 'weight'):
+            if self.weight.type() != input.type():
+                self.weight = self.weight.type_as(input)
+                recall = recall * self.weight * C  # (N, C)
+        recall_loss = 1 - torch.mean(recall)  # 1
+
+        return recall_loss
+
+if __name__ == '__main__':
+    y_target = torch.Tensor([[0, 1], [1, 0]]).long().cuda(0)
+    y_predict = torch.Tensor([[[1.5, 1.0], [2.8, 1.6]],
+                           [[1.0, 1.0], [2.4, 0.3]]]
+                          ).cuda(0)
+
+    criterion = RecallLoss(weight=[1, 1])
+    loss = criterion(y_predict, y_target)
+    print(loss)
+
+def make_one_hot(input, num_classes):
+    """Convert class index tensor to one hot encoding tensor.
+    Args:
+         input: A tensor of shape [N, 1, *]
+         num_classes: An int of number of class
+    Returns:
+        A tensor of shape [N, num_classes, *]
+    """
+    shape = np.array(input.shape)
+    shape[1] = num_classes
+    shape = tuple(shape)
+    result = torch.zeros(shape)
+    result = result.scatter_(1, input.cpu(), 1)
+
+    return result
+    
+class CrossEntropy(nn.Module):
+    def __init__(self, ignore_label=0, weight=None):
+        super(CrossEntropy, self).__init__()
+        self.ignore_label = ignore_label
+        self.criterion = nn.CrossEntropyLoss(
+            weight=weight,
+            ignore_index=ignore_label
+        )
+
+    def _forward(self, score, target):
+        print("ce-score.size():",score.shape)
+        print("ce-target.size():",target.shape)
+        ph, pw = score.size(1), score.size(2)
+        h, w = target.size(1), target.size(2)
+        if ph != h or pw != w:
+            score = F.interpolate(input=score, size=(
+                h, w), mode='bilinear', align_corners=True)
+#        score=np.round(score.cpu().detach().numpy()).astype(np.uint8).cuda()
+        loss = self.criterion(score, target)
+
+        return loss
+
+    def forward(self, score, target):
+        print("ce-score:",score)
+        print("ce-target:", target)
+        print("ce-len(score):", len(score))
+#        score = [score]
+#        score=np.round(score.cpu().detach().numpy()).astype(np.uint8)
+        weights = [1]*8
+        assert len(weights) == len(score)
+
+        return sum([w * self._forward(x, target) for (w, x) in zip(weights, score)])
+
+class BinaryDiceLoss(nn.Module):
+    """Dice loss of binary class
+    Args:
+        smooth: A float number to smooth loss, and avoid NaN error, default: 1
+        p: Denominator value: \sum{x^p} + \sum{y^p}, default: 2
+        predict: A tensor of shape [N, *]
+        target: A tensor of shape same with predict
+        reduction: Reduction method to apply, return mean over batch if 'mean',
+            return sum if 'sum', return a tensor of shape [N,] if 'none'
+    Returns:
+        Loss tensor according to arg reduction
+    Raise:
+        Exception if unexpected reduction
+    """
+    def __init__(self, smooth=1, p=2, reduction='mean'):
+        super(BinaryDiceLoss, self).__init__()
+        self.smooth = smooth
+        self.p = p
+        self.reduction = reduction
+
+    def forward(self, predict, target):
+#        print(predict.shape)
+#        print(target.shape)
+        assert predict.shape[0] == target.shape[0], "predict & target batch size don't match"
+        predict = predict.contiguous().view(predict.shape[0], -1)
+        target = target.contiguous().view(target.shape[0], -1)
+
+        num = torch.sum(torch.mul(predict, target), dim=1) + self.smooth
+        den = torch.sum(predict.pow(self.p) + target.pow(self.p), dim=1) + self.smooth
+
+        loss = 1 - num / den
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        elif self.reduction == 'none':
+            return loss
+        else:
+            raise Exception('Unexpected reduction {}'.format(self.reduction))
+
+
+class DiceLoss(nn.Module):
+    """Dice loss, need one hot encode input
+    Args:
+        weight: An array of shape [num_classes,]
+        ignore_index: class index to ignore
+        predict: A tensor of shape [N, C, *]
+        target: A tensor of same shape with predict
+        other args pass to BinaryDiceLoss
+    Return:
+        same as BinaryDiceLoss
+    """
+    def __init__(self, weight=None, ignore_index=None, **kwargs):
+        super(DiceLoss, self).__init__()
+        self.kwargs = kwargs
+        self.weight = weight
+        self.ignore_index = ignore_index
+
+    def forward(self, predict, target):
+#        assert predict.shape == target.shape, 'predict & target shape do not match'
+#        print("target.shape[1]",target.shape[1])
+        dice = BinaryDiceLoss(**self.kwargs)
+        total_loss = 0
+        predict = F.softmax(predict, dim=1)
+        for i in range(predict.shape[1]):
+            
+            if i != self.ignore_index:
+                          
+                dice_loss = dice(predict[:, i], target)
+                if self.weight is not None:
+                    assert self.weight.shape[0] == target.shape[1], \
+                        'Expect weight shape [{}], get[{}]'.format(target.shape[1], self.weight.shape[0])
+                    dice_loss *= self.weights[i]
+                total_loss += dice_loss
+
+        return total_loss/target.shape[1]
+
+        
+class SoftMaxwithLoss(Module):
+    """
+    This function returns cross entropy loss for semantic segmentation
+    """
+
+    def __init__(self):
+        super(SoftMaxwithLoss, self).__init__()
+        self.softmax = nn.LogSoftmax(dim=1)
+        self.criterion = nn.NLLLoss(ignore_index=1)
+
+    def forward(self, out, label):
+        assert not label.requires_grad
+
+        loss = self.criterion(self.softmax(out), label.long())
+
+        return loss
+
+
+class BalancedCrossEntropyLoss(Module):
+    """
+    Balanced Cross Entropy Loss with optional ignore regions
+    """
+
+    def __init__(self, size_average=True, batch_average=True, pos_weight=None):
+        super(BalancedCrossEntropyLoss, self).__init__()
+        self.size_average = size_average
+        self.batch_average = batch_average
+        self.pos_weight = pos_weight
+
+    def forward(self, output, label, void_pixels=None):
+        assert (output.size() == label.size())
+        labels = torch.ge(label, 0.5).float()
+
+        # Weighting of the loss, default is HED-style
+        if self.pos_weight is None:
+            num_labels_pos = torch.sum(labels)
+            num_labels_neg = torch.sum(1.0 - labels)
+            num_total = num_labels_pos + num_labels_neg
+            w = num_labels_neg / num_total
+        else:
+            w = self.pos_weight
+
+        output_gt_zero = torch.ge(output, 0).float()
+        loss_val = torch.mul(output, (labels - output_gt_zero)) - torch.log(
+            1 + torch.exp(output - 2 * torch.mul(output, output_gt_zero)))
+
+        loss_pos_pix = -torch.mul(labels, loss_val)
+        loss_neg_pix = -torch.mul(1.0 - labels, loss_val)
+
+        if void_pixels is not None and not self.pos_weight:
+            w_void = torch.le(void_pixels, 0.5).float()
+            loss_pos_pix = torch.mul(w_void, loss_pos_pix)
+            loss_neg_pix = torch.mul(w_void, loss_neg_pix)
+            num_total = num_total - torch.ge(void_pixels, 0.5).float().sum()
+            w = num_labels_neg / num_total
+
+        loss_pos = torch.sum(loss_pos_pix)
+        loss_neg = torch.sum(loss_neg_pix)
+
+        final_loss = w * loss_pos + (1 - w) * loss_neg
+
+        if self.size_average:
+            final_loss /= float(np.prod(label.size()))
+        elif self.batch_average:
+            final_loss /= label.size()[0]
+
+        return final_loss
+
+
+class BinaryCrossEntropyLoss(Module):
+    """
+    Binary Cross Entropy with ignore regions, not balanced.
+    """
+
+    def __init__(self, size_average=True, batch_average=True):
+        super(BinaryCrossEntropyLoss, self).__init__()
+        self.size_average = size_average
+        self.batch_average = batch_average
+
+    def forward(self, output, label, void_pixels=None):
+        assert (output.size() == label.size())
+
+        labels = torch.ge(label, 0.5).float()
+
+        output_gt_zero = torch.ge(output, 0).float()
+        loss_val = torch.mul(output, (labels - output_gt_zero)) - torch.log(
+            1 + torch.exp(output - 2 * torch.mul(output, output_gt_zero)))
+
+        loss_pos_pix = -torch.mul(labels, loss_val)
+        loss_neg_pix = -torch.mul(1.0 - labels, loss_val)
+
+        if void_pixels is not None:
+            w_void = torch.le(void_pixels, 0.5).float()
+            loss_pos_pix = torch.mul(w_void, loss_pos_pix)
+            loss_neg_pix = torch.mul(w_void, loss_neg_pix)
+
+        loss_pos = torch.sum(loss_pos_pix)
+        loss_neg = torch.sum(loss_neg_pix)
+        final_loss = loss_pos + loss_neg
+
+        if self.size_average:
+            final_loss /= float(np.prod(label.size()))
+        elif self.batch_average:
+            final_loss /= label.size()[0]
+
+        return final_loss
+
+
+class DepthLoss(nn.Module):
+    """
+    Loss for depth prediction. By default L1 loss is used.  
+    """
+    def __init__(self, loss='l1'):
+        super(DepthLoss, self).__init__()
+        if loss == 'l1':
+            self.loss = nn.L1Loss()
+
+        else:
+            raise NotImplementedError('Loss {} currently not supported in DepthLoss'.format(loss))
+
+    def forward(self, out, label):
+        mask = (label != 255)
+        return self.loss(torch.masked_select(out, mask), torch.masked_select(label, mask))
+
+
+class Normalize(nn.Module):
+    def __init__(self):
+        super(Normalize, self).__init__()
+
+    def forward(self, bottom):
+        qn = torch.norm(bottom, p=2, dim=1).unsqueeze(dim=1) + 1e-12
+        top = bottom.div(qn)
+
+        return top
+
+
+class NormalsLoss(Module):
+    """
+    L1 loss with ignore labels
+    normalize: normalization for surface normals
+    """
+    def __init__(self, size_average=True, normalize=False, norm=1):
+        super(NormalsLoss, self).__init__()
+
+        self.size_average = size_average
+
+        if normalize:
+            self.normalize = Normalize()
+        else:
+            self.normalize = None
+
+        if norm == 1:
+            print('Using L1 loss for surface normals')
+            self.loss_func = F.l1_loss
+        elif norm == 2:
+            print('Using L2 loss for surface normals')
+            self.loss_func = F.mse_loss
+        else:
+            raise NotImplementedError
+
+    def forward(self, out, label, ignore_label=255):
+        assert not label.requires_grad
+        mask = (label != ignore_label)
+        n_valid = torch.sum(mask).item()
+
+        if self.normalize is not None:
+            out_norm = self.normalize(out)
+            loss = self.loss_func(torch.masked_select(out_norm, mask), torch.masked_select(label, mask), reduction='sum')
+        else:
+            loss = self.loss_func(torch.masked_select(out, mask), torch.masked_select(label, mask), reduction='sum')
+
+        if self.size_average:
+            if ignore_label:
+                ret_loss = torch.div(loss, max(n_valid, 1e-6))
+                return ret_loss
+            else:
+                ret_loss = torch.div(loss, float(np.prod(label.size())))
+                return ret_loss
+
+        return loss
